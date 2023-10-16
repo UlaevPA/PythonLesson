6560b7bd74cf62815af5cd4c7bd5ff581cdbdbb1
ulaev <ulaev1990@mail.ru>
2023-10-16 22:07:46 +0300

    Add task15.1

--- Lesson2/task15.py
+++ Lesson2/task15.py
@@ -12,13 +12,33 @@
 # Input: 5 -> 5 1 6 5 9
 # Output: 1 9
 
-import random
-n = int(input("Введите количество арбузов: "))
+# import random
+# n = int(input("Введите количество арбузов: "))
 
-massa = [random.randint(1, 25) for i in range (n)]
-print(massa)    
-min_weight = min(massa)
-max_weight = max(massa)
-print(f"Для тещи: ", min_weight, "кг")
-print(f"Для себя: ", max_weight, "кг")
+# massa = [random.randint(1, 25) for i in range (n)]
+# print(massa)    
+# min_weight = min(massa)
+# max_weight = max(massa)
+# print(f"Для тещи: ", min_weight, "кг")
+# print(f"Для себя: ", max_weight, "кг")
+
+import random
+# print(random.random())
+# print(random.randint(1, 2))
+# print('-' * 45)
+n = 5
+mass_list = []
+for _ in range(n):
+    mass_list.append(random.randint(1, 10))
+print(mass_list)
+# print(' *', min(mass_list), max(mass_list))
+# print(sum(mass_list))
+min_ = mass_list[0]
+max_ = mass_list[0]
+for i in mass_list:
+    if i > max_:
+        max_ = i
+    if i < min_:
+        min_ = i
+print(min_, max_)
     
