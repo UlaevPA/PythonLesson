3e81ef9d4f89bb5bd167bee520d94a58eaf564b8
ulaev <ulaev1990@mail.ru>
2023-10-19 20:54:25 +0300

    Add task19

--- Lection3/task17.py
+++ Lection3/task17.py
@@ -3,13 +3,15 @@
 # встречается различных чисел.
 # Input: [1, 1, 2, 0, -1, 3, 4, 4]
 # Output: 6
-from random import randint
-list = []
+list_num = [1, 1, 2, 0, -1, 3, 4, 4]
+print(len(set(list_num)))
+print(set(list_num))
+
+list_num = [1, 1, 2, 0, -1, 3, 4, 4, 4, 487]
 count = 0
-for i in range(7):
-    list.append(randint(-10, 10))
-print(list)
-for i in list:
-    if list[0] != list:
-        count += 1
-print("Встречается", count-1, "разных чисел")
+new_list = []
+for i in list_num:
+    if i not in new_list:
+        new_list.append(i)
+print(len(new_list))
+print(new_list)
