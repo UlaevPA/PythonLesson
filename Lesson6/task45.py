a1c42db9c36fb8d66a7cfde40c9b509cbab56216
ulaev <ulaev1990@mail.ru>
2023-11-13 21:56:32 +0300

    Add task 45.1

--- Lesson6/task45.py
+++ Lesson6/task45.py
@@ -14,25 +14,43 @@
 # выведена только один раз (перестановка чисел новую
 # пару не дает).
 
-def sum_div(_num_):
-    sum = 0
-    for i in range(1, _num_ // 2 + 1):
-        if _num_ % i == 0:
-            sum += i
-    return sum
+# def sum_div(_num_):
+#     sum = 0
+#     for i in range(1, _num_ // 2 + 1):
+#         if _num_ % i == 0:
+#             sum += i
+#     return sum
 
-def main():
-    user_num = 100001
-    while user_num > 100000:
-        user_num = int(input("Введите предельное число: "))
-        result_list = []
-        for i in range(1, user_num):
-            sum1 = sum_div(i)
-            sum2 = sum_div(sum1)
-            if i == sum2 and i != sum1:
-                if {sum1, i} in result_list:
-                    continue
-                result_list.append({sum1, i})
-        return result_list
+# def main():
+#     user_num = 100001
+#     while user_num > 100000:
+#         user_num = int(input("Введите предельное число: "))
+#         result_list = []
+#         for i in range(1, user_num):
+#             sum1 = sum_div(i)
+#             sum2 = sum_div(sum1)
+#             if i == sum2 and i != sum1:
+#                 if {sum1, i} in result_list:
+#                     continue
+#                 result_list.append({sum1, i})
+#         return result_list
 
-print(main())
+# print(main())
+
+def sumDivisitors(num):
+    div = (i for i in range(1, int(num / 2) + 1) if num % i == 0)
+    # sum_ = sum(list(div))
+    return sum(div)
+
+def friendlyNums(num):
+    newArray = (sorted([i,sumDivisitors(i)]) for i in range(4, num + 1)
+                     if i == sumDivisitors(sumDivisitors(i)) and i !=sumDivisitors(i))
+    friendlyArray = dict(newArray)
+    return friendlyArray
+
+def printFriendlyDict(dict_):
+    for k,v in dict_.items():
+        print(k,v)
+
+k = int(input('Ведите число до которого будет осуществляться поиск: '))
+printFriendlyDict(friendlyNums(k))
